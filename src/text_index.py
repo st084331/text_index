0c1bdc8588a784854045f163e13039e6904e5039
st084331 <st084331@student.spbu.ru>
2020-10-09 17:30:28 +0300

    version 0.0.2

--- src/text_index.py
+++ src/text_index.py
@@ -10,6 +10,19 @@ def parser_args():
     p.add_argument('--word', type=str)
     return p
 
+def page_count(text):
+    demo_text = text + '\n'
+    pages = 0
+    while demo_text != '':
+        str_end = demo_text.index('\n')
+        pages += 1
+        demo_text = demo_text[(str_end + 1):]
+    return pages
+
+def word_places(text):
+    places = []
+    demo_text = text.replace()
+
 parser = parser_args()
 #word = parser.parse_args().word
 #mode = parser.parse_args().mode
@@ -21,5 +34,5 @@ file = open(filepath.replace('.', '/Users/akabynda/pyhonProject/text_index/data'
 #for Windows
 text = file.read()
 print(text)
-#demo_text = text
-pages = 0
+pages = page_count(text)
+print(pages)
