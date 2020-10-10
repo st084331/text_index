6b359db0438889eb3d95ab341c2aeb4292daa999
st084331 <st084331@student.spbu.ru>
2020-10-11 00:58:03 +0300

    Word finding

--- src/text_index.py
+++ src/text_index.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
 import argparse
+import collections
+from collections import Counter
+import re
+import string
+import io
 import codecs
 import os
 
@@ -8,31 +13,53 @@ def parser_args():
     p.add_argument('--file', type=str)
     p.add_argument('--mode', type=str)
     p.add_argument('--word', type=str)
+    p.add_argument('--num', type=str)
     return p
 
-def page_count(text):
-    demo_text = text + '\n'
-    pages = 0
+
+def find_word(text, word):
+    e = '.', ',', ' " ', ';', ':', '-', '!', '?'
+    demo_text = text.lower() + '\n'
+    for i in range(len(e)):
+        demo_text = demo_text.replace(e[i], "")
+    print(demo_text)
+    page = 1
+    pages = []
+    word = ' '+word+' '
+    counter = 0
     while demo_text != '':
         str_end = demo_text.index('\n')
-        pages += 1
+        str_page = ' ' + demo_text[:str_end] + ' '
+        print(str_page)
+        if (str_page.find(word.decode('utf - 8')) != -1):
+            bul = 0
+            for i in pages:
+                if(i == page):
+                    bul = 1
+            if (bul == 0):
+                pages.append(page)
+        if counter == 45:
+            counter = 0
+            page += 1
+        counter += 1
         demo_text = demo_text[(str_end + 1):]
-    return pages
+    print(word.decode('utf-8').replace(' ', ''))
+    print(pages)
 
-def word_places(text):
-    places = []
-    demo_text = text.replace()
+def common_words(text):
+    text = re.sub(r'[^\w\s]','',text).lower()
+    words = text.split()
+
 
 parser = parser_args()
-#word = parser.parse_args().word
+word = parser.parse_args().word
 #mode = parser.parse_args().mode
 name_file = parser.parse_args().file + '.txt'
+#for Windows
+#file = open(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', name_file))
 #for Mac
-dirpath = os.path.dirname(__file__)
-filepath = os.path.join(dirpath, name_file)
-file = open(filepath.replace('.', '/Users/akabynda/pyhonProject/text_index/data', 1))
+file = io.open('/Users/akabynda/pyhonProject/text_index/data/' + name_file, encoding="utf - 8")
 #for Windows
 text = file.read()
-print(text)
-pages = page_count(text)
-print(pages)
+print(file)
+find_word(text, word)
